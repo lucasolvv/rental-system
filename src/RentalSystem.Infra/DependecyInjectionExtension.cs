8a704715c919925c2ec0ecabf22d84e4743642bc
Lucas <lvksounds@gmail.com>
2025-07-31 15:04:23 -0300

    feat: criando container de injeção de dependencia da camada de infra

---
+++ src/RentalSystem.Infra/DependecyInjectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentalSystem.Domain.Contracts.MotoContracts;
+using RentalSystem.Infra.DataAccess;
+using RentalSystem.Infra.DataAccess.Repositories;
+
+namespace RentalSystem.Infra
+{
+    public static class DependecyInjectionExtension
+    {
+        public static void AddInfraDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext_PostGrees(services, configuration);
+            AddRepositories(services);
+        }
+
+        private static void AddDbContext_PostGrees(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RentalSystemDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IMotoWriteOnlyContract, MotoRepository>();
+            services.AddScoped<IMotoReadOnlyContract, MotoRepository>();
+        }
+
+    }
+}
