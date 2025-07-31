795967c32d018db6b271960a540312ac88577946
Lucas <lvksounds@gmail.com>
2025-07-31 15:01:28 -0300

    feat: adicionando contratos de read e write para classe de moto

---
+++ src/RentalSystem.Domain/Contracts/MotoContracts/IMotoWriteOnlyContract.cs
@@ -0,0 +1,10 @@
+﻿using RentalSystem.Domain.Entities;
+namespace RentalSystem.Domain.Contracts.MotoContracts
+{
+    public interface IMotoWriteOnlyContract
+    {
+        public Task CreateMotoAsync(Moto moto);
+        public Task UpdatePlacaByIdAsync(Guid id, string novaPlaca);
+        public Task DeleteMotoByIdAsync(Guid id);
+    }
+}
