9d479b60e88665184ac84aba8d8acca6e5110007
Lucas Oliveira <lvksounds@gmail.com>
2025-07-30 22:15:35 -0300

    Feat: Adicionando entidades base ja com dataAnnotations

---
+++ src/RentalSystem.Domain/Entities/EventoMotoCadastrada.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalSystem.Domain.Entities
+{
+    public class EventoMotoCadastrada
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid MotoId { get; set; }
+
+        [Required]
+        public DateTime DataEvento { get; set; }
+
+        public string? ConteudoMensagem { get; set; } // JSON, texto ou log simples
+    }
+}
---
+++ src/RentalSystem.Domain/Entities/Moto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace RentalSystem.Domain.Entities
+{
+    public class Moto
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public int Ano { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Placa { get; set; }
+    }
+}
