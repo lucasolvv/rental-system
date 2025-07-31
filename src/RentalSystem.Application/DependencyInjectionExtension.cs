099579e83c7a563850e8f6f44411a47941136232
Lucas <lvksounds@gmail.com>
2025-07-31 14:59:47 -0300

    Feat: adicionando container de injesção de dependencia da camada de application

---
+++ src/RentalSystem.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RentalSystem.Application.Services.AutoMapper;
+using RentalSystem.Application.UseCases.Motos.CreateMotoUseCases;
+namespace RentalSystem.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplicationDependencies(this IServiceCollection services)
+        {
+            AddUseCases(services);
+            AddAutoMapper(services);
+        }
+
+        private static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddScoped(option => new AutoMapper.MapperConfiguration(option =>
+            {
+                option.AddProfile(new AutoMapping());
+            }).CreateMapper());
+        }
+
+        private static void AddUseCases(IServiceCollection services)
+        {
+            services.AddScoped<ICreateMotoUseCase, CreateMotoUseCase>();
+        }
+
+    }
+}
