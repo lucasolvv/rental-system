40a9360dca9c7cfb01e534462abc3000e8ffff37
Lucas <lvksounds@gmail.com>
2025-07-31 14:51:42 -0300

    feat: Adicionando auto mapper para mapear classes de comunicação com entidades

---
+++ src/RentalSystem.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RentalSystem.Communication.Requests.Motos;
+using RentalSystem.Domain.Entities;
+
+namespace RentalSystem.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        { 
+            RequestToDomain();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<RequestCreateMotoJson, Moto>();
+        }
+    }
+}
