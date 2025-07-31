09c7a371127aae0d2dceb9da01187d3610cedfeb
Lucas <lvksounds@gmail.com>
2025-07-31 15:04:41 -0300

    feat: adicionando controller pro dominio das motos

---
+++ src/RentalSystem.Presentation/Controllers/MotoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentalSystem.Application.UseCases.Motos.CreateMotoUseCases;
+using RentalSystem.Communication.Requests.Motos;
+
+namespace RentalSystem.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotoController : ControllerBase
+    {
+        [HttpPost("/motos")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateMoto([FromBody] RequestCreateMotoJson request, [FromServices] ICreateMotoUseCase useCase)
+        {
+            await useCase.ExecuteAsync(request);
+            return Created();
+        }
+    }
+}
