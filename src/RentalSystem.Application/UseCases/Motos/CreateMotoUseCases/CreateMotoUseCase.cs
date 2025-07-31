15915ff417d4a04b71b7d820920a2a4805e5fe1d
Lucas <lvksounds@gmail.com>
2025-07-31 14:59:07 -0300

    Feat: criando UseCase de criação de moto

---
+++ src/RentalSystem.Application/UseCases/Motos/CreateMotoUseCases/CreateMotoUseCase.cs
@@ -0,0 +1,39 @@
+﻿using RentalSystem.Domain.Contracts.MotoContracts;
+using AutoMapper;
+using RentalSystem.Communication.Requests.Motos;
+namespace RentalSystem.Application.UseCases.Motos.CreateMotoUseCases
+{
+    public class CreateMotoUseCase : ICreateMotoUseCase
+    {
+        private readonly IMotoWriteOnlyContract _motoWriteRepository;
+        private readonly IMotoReadOnlyContract _motoReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public CreateMotoUseCase(IMotoWriteOnlyContract motoWriteRepository, 
+                                 IMotoReadOnlyContract motoReadOnlyRepository, 
+                                 IMapper mapper)
+        {
+            _motoWriteRepository = motoWriteRepository;
+            _motoReadOnlyRepository = motoReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task ExecuteAsync(RequestCreateMotoJson request)
+        {
+            await Validate(request);
+            
+            var moto = _mapper.Map<Domain.Entities.Moto>(request);
+            
+            await _motoWriteRepository.CreateMotoAsync(moto);
+        }
+
+        private async Task Validate (RequestCreateMotoJson request)
+        {
+            var motoExists = await _motoReadOnlyRepository.MotoAlreadyExists(request.Placa);
+            
+            if (motoExists) throw new ArgumentException("Moto with the same placa already exists.");
+            
+            
+        }
+    }
+}
---
+++ src/RentalSystem.Application/UseCases/Motos/CreateMotoUseCases/ICreateMotoUseCase.cs
@@ -0,0 +1,8 @@
+﻿using RentalSystem.Communication.Requests.Motos;
+namespace RentalSystem.Application.UseCases.Motos.CreateMotoUseCases
+{
+    public interface ICreateMotoUseCase
+    {
+        Task ExecuteAsync(RequestCreateMotoJson request);
+    }
+}
