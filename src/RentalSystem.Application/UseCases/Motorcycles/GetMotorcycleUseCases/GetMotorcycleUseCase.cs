db3a59c3c1e26db24bb691bb2fd53759d096a9e7
Lucas <lvksounds@gmail.com>
2025-08-01 14:35:05 -0300

    feat: adicionando classe UseCase de get de motos

---
+++ src/RentalSystem.Application/UseCases/Motorcycles/GetMotorcycleUseCases/GetMotorcycleUseCase.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using RentalSystem.Communication.Requests.Motorcycles;
+using RentalSystem.Communication.Responses;
+using RentalSystem.Domain.Contracts.MotorcycleContracts;
+
+namespace RentalSystem.Application.UseCases.Motorcycles.GetMotorcycleUseCases
+{
+    public class GetMotorcycleUseCase : IGetMotorcycleUseCase
+    {
+        private readonly IMotorcycleReadOnlyContract _motorcycleReadOnlyRepository;
+        private readonly IMapper _mapper;
+
+        public GetMotorcycleUseCase(IMotorcycleReadOnlyContract motorcycleReadOnlyRepository, IMapper mapper)
+        {
+            _motorcycleReadOnlyRepository = motorcycleReadOnlyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ResponseGetMotorcycleJson>> ExecuteAsync(RequestGetMotorcycleByPlateJson request)
+        {
+            if (string.IsNullOrEmpty(request.Placa))
+            {
+                var allMotorcycles = await _motorcycleReadOnlyRepository.GetAllMotorcyclesAsync();
+                return _mapper.Map<IEnumerable<ResponseGetMotorcycleJson>>(allMotorcycles);
+            }
+
+            var motorcycles = await _motorcycleReadOnlyRepository.GetMotorcycleByPlateAsync(request.Placa);
+            return _mapper.Map<IEnumerable<ResponseGetMotorcycleJson>>(motorcycles);
+
+        }
+    }
+}
