5f06c4a5c9cb611af2f5ca24237bbc3e963083f5
Lucas <lvksounds@gmail.com>
2025-08-01 14:35:27 -0300

    fix: acertando idioma da classe

--- src/RentalSystem.Application/UseCases/Motorcycles/GetMotorcycleUseCases/IGetMotorcycleUseCase.cs
+++ src/RentalSystem.Application/UseCases/Motorcycles/GetMotorcycleUseCases/IGetMotorcycleUseCase.cs
@@ -1,6 +1,10 @@
-﻿namespace RentalSystem.Application.UseCases.Motorcycles.GetMotorcycleUseCases
+﻿using RentalSystem.Domain.Entities;
+using RentalSystem.Communication.Requests.Motorcycles;
+using RentalSystem.Communication.Responses;
+namespace RentalSystem.Application.UseCases.Motorcycles.GetMotorcycleUseCases
 {
-    public interface IGetMotoUseCases
+    public interface IGetMotorcycleUseCase
     {
+        Task<IEnumerable<ResponseGetMotorcycleJson>> ExecuteAsync(RequestGetMotorcycleByPlateJson request);
     }
 }
