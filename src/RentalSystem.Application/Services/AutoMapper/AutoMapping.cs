4fb8d5ff8647af88244c0678ac81e25cc59a7b68
Lucas <lvksounds@gmail.com>
2025-08-01 14:34:06 -0300

    fix: atualizando mapeamento pois idioma das entidades nao é mais o mesmo das classes de comunicação (jsons)

--- src/RentalSystem.Application/Services/AutoMapper/AutoMapping.cs
+++ src/RentalSystem.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
-using RentalSystem.Communication.Requests.Motorcycle;
+using RentalSystem.Communication.Requests.Motorcycles;
+using RentalSystem.Communication.Responses;
 using RentalSystem.Domain.Entities;
 
 namespace RentalSystem.Application.Services.AutoMapper
@@ -7,13 +8,26 @@ namespace RentalSystem.Application.Services.AutoMapper
     public class AutoMapping : Profile
     {
         public AutoMapping()
-        { 
+        {
             RequestToDomain();
+            DomainToResponse();
         }
 
         private void RequestToDomain()
         {
-            CreateMap<RequestCreateMotorcycleJson, Motorcycle>();
+            CreateMap<RequestCreateMotorcycleJson, Motorcycle>()
+                .ForMember(dest => dest.Year, opt => opt.MapFrom(src => src.Ano))
+                .ForMember(dest => dest.Model, opt => opt.MapFrom(src => src.Modelo))
+                .ForMember(dest => dest.LicensePlate, opt => opt.MapFrom(src => src.Placa));
+        }
+
+        private void DomainToResponse()
+        {
+            CreateMap<Motorcycle, ResponseGetMotorcycleJson>()
+                .ForMember(dest => dest.Identificador, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Ano, opt => opt.MapFrom(src => src.Year))
+                .ForMember(dest => dest.Modelo, opt => opt.MapFrom(src => src.Model))
+                .ForMember(dest => dest.Placa, opt => opt.MapFrom(src => src.LicensePlate));
         }
     }
 }
