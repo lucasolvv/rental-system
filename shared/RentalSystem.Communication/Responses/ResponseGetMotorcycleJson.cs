d141521718cfb90d1c03684be71900c94a3ffb2c
Lucas <lvksounds@gmail.com>
2025-08-01 14:32:39 -0300

    feat: adicionando json de get pra motos

---
+++ shared/RentalSystem.Communication/Responses/ResponseGetMotorcycleJson.cs
@@ -0,0 +1,10 @@
+﻿namespace RentalSystem.Communication.Responses
+{
+    public class ResponseGetMotorcycleJson
+    {
+        public string Identificador { get; set; }
+        public int Ano { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+    }
+}
