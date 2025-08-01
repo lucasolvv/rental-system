e413b436af15f19e480097f69442671b307b7222
Lucas <lvksounds@gmail.com>
2025-08-01 14:33:20 -0300

    feat: adicionando uma exception de moto nao encontrada pra usos futuros

---
+++ shared/RentalSystem.Exceptions/ExceptionBase/MotorcycleNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace RentalSystem.Exceptions.ExceptionBase
+{
+    public class MotorcycleNotFoundException : RentalSystemException
+    {
+        private readonly string _errorMessage;
+        public MotorcycleNotFoundException(string errorMessage) : base(string.Empty)
+        {
+            _errorMessage = errorMessage;
+        }
+        public override string GetErrorMessage() => _errorMessage;
+        public override System.Net.HttpStatusCode GetStatusCode() => System.Net.HttpStatusCode.NotFound;
+    }
+}
