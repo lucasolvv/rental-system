240e7454e6bc6c3b75b6ab9b8c3898b40419c478
Lucas <lvksounds@gmail.com>
2025-08-01 14:32:12 -0300

    Feat: * adicionando DataAnnotation com limite de caracteres para o filtro da placa; * Alterando tipo do campo de ano de string pra int

--- shared/RentalSystem.Communication/Requests/Motorcycles/RequestGetMotocycleByPlateJson.cs
+++ shared/RentalSystem.Communication/Requests/Motorcycles/RequestGetMotocycleByPlateJson.cs
@@ -1,7 +1,10 @@
-﻿namespace RentalSystem.Communication.Requests.Motorcycles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalSystem.Communication.Requests.Motorcycles
 {
-    public class RequestMotorcycleByPlacaJson
+    public class RequestGetMotorcycleByPlateJson
     {
+        [StringLength(8, ErrorMessage = "A placa não pode ter mais de 8 caracteres.")]
         public string? Placa { get; set; } = string.Empty;
     }
 }
--- shared/RentalSystem.Communication/Requests/Motorcycles/ResquestCreateMotorcycleJson.cs
+++ shared/RentalSystem.Communication/Requests/Motorcycles/ResquestCreateMotorcycleJson.cs
@@ -1,9 +1,13 @@
-﻿namespace RentalSystem.Communication.Requests.Motorcycle
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalSystem.Communication.Requests.Motorcycles
 {
     public class RequestCreateMotorcycleJson
     {
-        public string Ano { get; set; }
+        public int Ano { get; set; }
         public string Modelo { get; set; }
+
+        [StringLength(7, ErrorMessage = "A placa não pode ter mais de 7 caracteres.")]
         public string Placa { get; set; }
     }
 }
