6b7c65c4be4ce74467ff321e24b68d3c0b95eb7c
Lucas Oliveira <lvksounds@gmail.com>
2025-08-02 12:57:22 -0300

    refactor: refinando metodo de delete, adicionando metodo de validade para validar casos de moto nao existor e id de moto nulos

--- src/RentalSystem.Application/UseCases/Motorcycles/DeleteMotorcycleUseCases/DeleteMotorcycleUseCase.cs
+++ src/RentalSystem.Application/UseCases/Motorcycles/DeleteMotorcycleUseCases/DeleteMotorcycleUseCase.cs
@@ -6,23 +6,35 @@ namespace RentalSystem.Application.UseCases.Motorcycles.DeleteMotorcycleUseCases
 {
     public class DeleteMotorcycleUseCase : IDeleteMotorcycleUseCase
     {
-        private readonly IMotorcycleDeleteOnlyRepository _repository;
+        private readonly IMotorcycleDeleteOnlyRepository _deleteRepository;
+        private readonly IMotorcycleReadOnlyRepository _readOnlyRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public DeleteMotorcycleUseCase(IMotorcycleDeleteOnlyRepository repository, IUnitOfWork unitOfWork)
+        public DeleteMotorcycleUseCase(IMotorcycleDeleteOnlyRepository repository, IMotorcycleReadOnlyRepository readOnlyRepository, IUnitOfWork unitOfWork)
         {
-            _repository = repository;
+            _deleteRepository = repository;
             _unitOfWork = unitOfWork;
+            _readOnlyRepository = readOnlyRepository;
         }
 
         public async Task ExecuteAsync(Guid id)
         {
-            if (id == Guid.Empty)
-            {
-                throw new ErrorOnValidationException($"{id} invalido");
-            }
-            await _repository.DeleteMotorcycle(id);
+
+            await Validate(id);
+            await _deleteRepository.DeleteMotorcycle(id);
             await _unitOfWork.Commit();
+        }
+
+
+        private async Task Validate(Guid id)
+        {
+            if (id == Guid.Empty) 
+                throw new ErrorOnValidationException($"{id} da moto não pode ser vazio.");
+                        
+            var motorcycle = await _readOnlyRepository.GetMotorcycleByIdAsync(id);
+
+            if (motorcycle is null) 
+                throw new ErrorOnValidationException($"Moto com id {id} não encontrada.");
         }
     }
 }
