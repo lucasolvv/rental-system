9de3d9fff5aec44e81487dfe4262580dc64f4d0e
Lucas Oliveira <lvksounds@gmail.com>
2025-08-02 15:54:29 -0300

    feat: adicionando interface de repo read only do entregador

---
+++ src/RentalSystem.Domain/Repositories/DeliveryDriver/IDeliveryDriverReadOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RentalSystem.Domain.Repositories.DeliveryDriver
+{
+    public interface IDeliveryDriverReadOnlyRepository
+    {
+        Task<bool> DeliveryDriverWithCnhAlreadyExists(string cnh);   
+        Task<bool> DeliveryDriverWithCnpjAlreadyExists(string cnpj);
+    }
+}
