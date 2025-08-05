be609c2048a1df617d9713fd4c1cd8a3424f2dca
Lucas <lvksounds@gmail.com>
2025-08-05 15:33:13 -0300

    feat: adicionando validação para caso de moto ja estar com alguma locação ativa

--- src/RentalSystem.Application/UseCases/Motorcycles/DeleteMotorcycleUseCases/DeleteMotorcycleUseCase.cs
+++ src/RentalSystem.Application/UseCases/Motorcycles/DeleteMotorcycleUseCases/DeleteMotorcycleUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using RentalSystem.Domain.Repositories;
 using RentalSystem.Domain.Repositories.Motorcycle;
+using RentalSystem.Domain.Repositories.Rental;
 using RentalSystem.Exceptions.ExceptionBase;
 
 namespace RentalSystem.Application.UseCases.Motorcycles.DeleteMotorcycleUseCases
@@ -8,13 +9,18 @@ namespace RentalSystem.Application.UseCases.Motorcycles.DeleteMotorcycleUseCases
     {
         private readonly IMotorcycleDeleteOnlyRepository _deleteRepository;
         private readonly IMotorcycleReadOnlyRepository _readOnlyRepository;
+        private readonly IRentalReadOnlyRepository _rentalReadOnlyRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public DeleteMotorcycleUseCase(IMotorcycleDeleteOnlyRepository repository, IMotorcycleReadOnlyRepository readOnlyRepository, IUnitOfWork unitOfWork)
+        public DeleteMotorcycleUseCase(IMotorcycleDeleteOnlyRepository repository, 
+            IMotorcycleReadOnlyRepository readOnlyRepository, 
+            IRentalReadOnlyRepository rentalReadOnlyRepository, 
+            IUnitOfWork unitOfWork)
         {
             _deleteRepository = repository;
             _unitOfWork = unitOfWork;
             _readOnlyRepository = readOnlyRepository;
+            _rentalReadOnlyRepository = rentalReadOnlyRepository;
         }
 
         public async Task ExecuteAsync(string id)
@@ -28,13 +34,20 @@ namespace RentalSystem.Application.UseCases.Motorcycles.DeleteMotorcycleUseCases
 
         private async Task Validate(string id)
         {
-            if (string.IsNullOrEmpty(id)) 
+            if (string.IsNullOrEmpty(id))
                 throw new ErrorOnValidationException($"{id} da moto não pode ser vazio.");
-                        
+
             var motorcycle = await _readOnlyRepository.GetMotorcycleByIdAsync(id);
 
-            if (motorcycle is null) 
+            if (motorcycle is null)
                 throw new ErrorOnValidationException($"Moto com id {id} não encontrada.");
+
+            var rental = await _rentalReadOnlyRepository.GetRentalByMotorcycleIdAsync(motorcycle.Id);
+
+            if (rental)
+            {
+                throw new ErrorOnValidationException($"Não é possível excluir a moto com id {id} porque ela está associada a uma locação.");
+            }
         }
     }
 }
